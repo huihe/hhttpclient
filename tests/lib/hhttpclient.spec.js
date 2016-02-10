0819cdaf13622423e5a2b2c62282e0036393e4f4
Hui He <hehuic@gmail.com>
2016-02-10 23:47:59 +1100

    added basic test

--- tests/lib/hhttpclient.spec.js
+++ tests/lib/hhttpclient.spec.js
@@ -1,12 +1,16 @@
+'use strict'
+var assert = require('assert');
 
 describe('hhttpclient.js', function(){
   
-  var hhttpclient = require('../../lib/hhttpclient');
+  var hhttp = require('../../lib/hhttpclient');
 
   describe('get', function(){
     it('should return ok', function(done){
-      hhttpclient().get('test.com').then(
+      hhttp.get('test.com').then(
         function(ok){
+          assert.equal(200, ok.statusCode)
+          console.log('body: ' + JSON.stringify(ok.body))
           done();
         })
     })
